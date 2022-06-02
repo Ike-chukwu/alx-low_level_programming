615e255fa10b1182d639e7c86bbb0361c56b2747
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-02 16:09:49 -0700

    task 444

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main-using puts function
+ *Return:always 0
  */
 int main(void)
 {
 
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = \"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
