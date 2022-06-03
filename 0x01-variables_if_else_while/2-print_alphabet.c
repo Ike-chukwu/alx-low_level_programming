bb3e848d2af878c473f9f4e2d33b45c2b45a64d9
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-03 15:48:04 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * * main - prints letter of the alphabet
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
