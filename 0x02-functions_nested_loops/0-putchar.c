7a9c1b7e17ee992cf7efd3a5bdbb445061bcd942
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-07 08:59:43 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-
 /**
- * main - Prints _putchar with putchar.
+ * main - Prints putchar.
  *
- * Return : Always 0 (Success)
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 	int lett [8] = {95, 112, 117, 116, 99, 104, 97, 114};
 
-	for (n = 0; n < 59; n++)
+	for (n = 0; n < 8; n++)
 	{
 		putchar(lett[n]);
 	}
