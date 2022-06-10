d435d3af084ec5ecc4b9ad27a1642fd06eddfc70
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-09 19:55:35 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+	int h;
+
+	for (h = 48; h < 58; h++)
+	{
+		if (h != 50 && h != 52)
+			_putchar(h);
+	}
+	_putchar('\n');
+}
