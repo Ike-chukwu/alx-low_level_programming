d33dc6d57b303ded28c4858ce0a461c03a068a84
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-09 20:11:08 -0700

    task 10again

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_triangle(int size);
+
+#endif
