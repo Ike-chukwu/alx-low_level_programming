babb687b3750535c3a409a8c9d4b14e0115eaecf
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-13 14:56:35 -0700

    task 00

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
-/** 
- * void reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98.
+/**
+ * reset_to_98 - takes a pointer to an int as param
  * @n: The pointer to an int.
  */
 void reset_to_98(int *n)
