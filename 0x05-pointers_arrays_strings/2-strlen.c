0327aa09e2c42fd7ebcc379746f145fb0f0f4a57
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-13 23:33:36 +0000

    yeahhh

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,8 @@
 #include <stddef.h>
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+i * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
  * Return: The length of @str.
  */
 int _strlen(char *s)
