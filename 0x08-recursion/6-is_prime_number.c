9526f30cd39a72706fcfb2d26d83011254433759
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-22 13:40:49 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+/**
+ * is_prime_number - prime
+ * @n: integer params
+ * Return: recursion
+ */
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
+}
