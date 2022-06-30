a764bf8fc9ad0aad90d0fd30c703c891b55c66b7
Ike-chukwu <iykeugboko9@gmail.com>
2022-06-30 21:18:28 +0000

    task 0

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc libmy.a *.o
-=======
-gcc -c *.c
-ar rc liball.a *.o
->>>>>>> dc136fb0b7b7260158152310b514f947a4e8a0ec
+ar -rc liball.a *.o
