8c079157a6e5df6a46bf65340ae9ccc1f4aa8fc8
Ike-chukwu <iykeugboko9@gmail.com>
2022-07-14 20:19:34 +0000

    task 00

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,9 @@
  * Desc: header file containinf prototypes and definitions
  * for all functions and types written in this proj
  */
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -27,5 +29,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *str);
 
 #endif /* LISTS_H */
